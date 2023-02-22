e19a3e71c83975add6831a53ff65145d8d00d5c9
Overlrd <premieraugust28@gmail.com>
2023-02-22 14:35:21 +0000

    fixed firebase called once

--- index.js
+++ index.js
@@ -1,51 +1,41 @@
+import http from 'http';
 import { GetWolfs, GetGoose } from './index.mjs';
 
-import http from 'http'
+const small_doc = { "/certification": "get certifications from firebase", "/projects": "get project from firebase" };
 
-
-let Infoarr = [] 
-const QuerySnapshot = await GetWolfs();
-QuerySnapshot.forEach((doc) => {
-    Infoarr.push({...doc.data()})
-    
-})
-
-let certarr = [] 
-const QuerySnapshot_again = await GetGoose();
-QuerySnapshot_again.forEach((doc) => {
-    certarr.push({...doc.data()})
-    
-})
-
-let small_doc = {"/certfication" : "get certifications from firebase", "/projects" : "get project from firebase"}
-
-
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/'){
+const server = http.createServer(async (req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
     res.statusCode = 200;
-    res.setHeader('Content-Type','application/json');
+    res.setHeader('Content-Type', 'application/json');
     res.write(JSON.stringify(small_doc));
     res.end();
-  }
-  else if (req.method === 'GET' && req.url === '/certification') {
+  } else if (req.method === 'GET' && req.url === '/certification') {
+    const certarr = [];
+    const querySnapshot = await GetGoose();
+    querySnapshot.forEach((doc) => {
+      certarr.push({ ...doc.data() });
+    });
     res.statusCode = 200;
     res.writeHead(200, {
-      'Content-Type':'application/json',
-      'Access-Control-Allow-Origin' : '*',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
-  });
+    });
     res.end(JSON.stringify(certarr));
-  }
-  else if (req.method === 'GET' && req.url === '/projects'){
+  } else if (req.method === 'GET' && req.url === '/projects') {
+    const infoarr = [];
+    const querySnapshot = await GetWolfs();
+    querySnapshot.forEach((doc) => {
+      infoarr.push({ ...doc.data() });
+    });
     res.statusCode = 200;
     res.writeHead(200, {
-      'Content-Type':'application/json',
-      'Access-Control-Allow-Origin' : '*',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
-  });
-    res.end(JSON.stringify(Infoarr));
-  }
-  else {
+    });
+    res.end(JSON.stringify(infoarr));
+  } else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
     res.end('404 Not Found\n');
